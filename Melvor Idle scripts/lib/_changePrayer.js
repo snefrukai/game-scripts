50acd0c63438b44f8d8771a683bf21a3cd60df45
Zhao Kai <snefrukai@gmail.com>
2022-06-23 19:30:21 +0800

    accept str

---
+++ Melvor Idle scripts/lib/_changePrayer.js
@@ -0,0 +1,28 @@
+window.changePrayer = function (activatePrayer = []) {
+  var arrTemp = []
+  for (const i of activatePrayer) {
+    if (typeof i == 'string') {
+      console.log(i)
+      arrTemp.push(Prayers[i])
+    }
+  }
+  // console.log(arrTemp)
+  activatePrayer = arrTemp
+
+  // ! from Action Queue
+  for (const i of player.activePrayers.entries()) {
+    // if target prayer is active, leave it unchangde
+    activatePrayer.includes(i[0])
+      ? activatePrayer.splice(
+          activatePrayer.findIndex((a) => a == i[0]),
+          1
+        )
+      : activatePrayer.unshift(i[0]) // adds to the beginning of array
+  }
+  for (const i of activatePrayer) {
+    player.togglePrayer(i) // in-game func
+  }
+}
+
+// changePrayer([0, 1]) // ! test
+changePrayer(['Thick_Skin']) // ! test

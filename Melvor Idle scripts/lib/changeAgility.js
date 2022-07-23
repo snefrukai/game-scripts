6beea97eaf091e591594956c564890c17f4718cb
Zhao Kai <snefrukai@gmail.com>
2022-07-23 14:27:32 +0800

    basic

---
+++ Melvor Idle scripts/lib/changeAgility.js
@@ -0,0 +1,96 @@
+window.agilitySetting = [
+  {
+    name: 'Thieving',
+    obstacle: {
+      0: 'Rope Climb',
+      4: 'Rooftop Run',
+    },
+    // obstacle: [
+    //   [0, 'Rope Climb'],
+    //   [4, 'Rooftop Run'],
+    // ],
+  },
+]
+
+window.autoChangeObstacle = function (skillTarget) {
+  // equip cape
+
+  // change ob if not as setting
+  var obCurrent = getObCurrent()
+  var obSetting = agilitySetting.find((e) =>
+    e.name.includes(skillTarget)
+  ).obstacle
+  // console.log(settingObstacle, obstacleCurrent)
+  for (const key in obSetting) {
+    if (obSetting[key] != obCurrent[key]) {
+      changeOb(key, obSetting[key])
+      console.log(`${key}: ${obSetting[key]}, done`)
+      // if using arr in seting, would be like:
+      // obstacleSetting[1] != obstacleCurrent[0]
+      // not very intuitive
+    }
+  }
+
+  // ========================================================================== //
+  //
+
+  function getObCurrent(params) {
+    var obCurrent = []
+    agilityObstacleMenus.forEach((e) => obCurrent.push(e.name.innerText))
+    return obCurrent
+  }
+
+  // setAgilityObstacle(0, 'Rope Climb') // ! test
+  // setAgilityObstacle(0, 'Cargo Net') // ! test
+  function changeOb(iCategory, obStr) {
+    // open ob menu
+    agilityObstacleMenus[iCategory].selectObstacleButton.click()
+
+    // select ob
+    var obObj = agilityObstacleSelectMenus.find(
+      (e) => e.name.innerText == obStr
+    )
+    obObj.link.click()
+
+    // v2
+    // no significant time improvement ?
+    // agilityObstacleSelectMenus.forEach((e) => {
+    //   if (e.name.innerText == obStr) {
+    //     e.link.click()
+    //     return true
+    //   }
+    // })
+
+    // confirm build
+    document.querySelector('.swal2-confirm.btn.btn-primary.m-1').click()
+  }
+}
+
+autoChangeObstacle('Thieving') // ! test
+
+// ========================================================================== //
+//
+
+// Action Queue
+
+// case 'Build Agility Obstacle': {
+//         const obstacleID = Agility.obstacles.findIndex(
+//           (a) => a.name == skillItem
+//         )
+//         const obstacleNumber = parseInt(actionName) - 1
+//         return () => {
+//           if (chosenAgilityObstacles[obstacleNumber] == obstacleID) return true
+//           if (
+//             !canIAffordThis(
+//               Agility.obstacles[obstacleID].cost,
+//               Agility.obstacles[obstacleID].skillRequirements,
+//               obstacleID
+//             )
+//           )
+//             return false
+//           if (chosenAgilityObstacles[obstacleNumber] >= 0)
+//             destroyAgilityObstacle(obstacleNumber, true)
+//           buildAgilityObstacle(obstacleID, true)
+//           return true
+//         }
+//       }
